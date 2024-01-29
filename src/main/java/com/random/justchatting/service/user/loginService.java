afc102e93540ac991683b6f67d35ef063e6eb8b2
hyeonjin-OH <mion0602@gmail.com>
2024-01-29 19:57:00 +0900

    Feat: Service domain 분리 작업

--- src/main/java/com/random/justchatting/service/user/loginService.java
+++ src/main/java/com/random/justchatting/service/user/loginService.java
@@ -1,42 +1,6 @@
 package com.random.justchatting.service.user;
 
-import com.random.justchatting.domain.login.User;
-import com.random.justchatting.domain.login.UserPrefer;
-import com.random.justchatting.domain.login.UserReq;
-import com.random.justchatting.repository.user.UserPreferRepository;
-import com.random.justchatting.repository.user.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+public interface LoginService {
 
-@Service
-@RequiredArgsConstructor
-public class loginService {
-
-    private final UserRepository userRepository;
-    private final UserPreferRepository userPreferRepository;
-
-    public Long saveApiKey(User user){
-
-        User findUser = userRepository.findByApiKey(user.getApiKey());
-
-        if (findUser != null){
-            return findUser.getId();
-        }
-        else{
-            User newUser = userRepository.save(user);
-            return newUser.getId();
-        }
-}
-
-    public UserPrefer getPreference(Long userId){
-
-        return userPreferRepository.findByUserId(userId);
-    }
-
-    public Long savePreference(UserPrefer prefer){
-        UserPrefer newPrefer = userPreferRepository.save(prefer);
-
-        return newPrefer.getUserId();
-    }
-
+    Long saveApiKey(Long userId);
 }
